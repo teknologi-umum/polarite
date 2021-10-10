1edf7e227a575b6290c33880b2ddc5f466fc50ed
Reinaldy Rafli <aldy505@tutanota.com>
2021-10-11 00:32:34 +0700

    test: created test cases

---
+++ business/controllers/insert_test.go
@@ -0,0 +1,61 @@
+package controllers_test
+
+import (
+	"context"
+	"polarite/business/controllers"
+	"polarite/business/models"
+	"testing"
+)
+
+func TestInsertPasteToDB(t *testing.T) {
+	defer TruncateTable(db, rds, mem)
+
+	p := controllers.PasteControllerImpl{
+		Cache:  rds,
+		Memory: mem,
+	}
+
+	c, err := db.Connx(context.Background())
+	if err != nil {
+		t.Error(err)
+	}
+	defer c.Close()
+
+	paste := models.Item{
+		Paste: "Hello world!",
+		Hash:  "7e81ebe9e604a0c97fef0e4cfe71f9ba0ecba13332bde953ad1c66e4",
+		IP:    "127.0.0.1",
+		User:  "example@test.com",
+	}
+
+	i, err := p.InsertPasteToDB(c, paste)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if i.ID == "" {
+		t.Error("something went wrong, got:", i)
+	}
+}
+
+func TestInsertPasteToCache(t *testing.T) {
+	defer TruncateTable(db, rds, mem)
+
+	p := controllers.PasteControllerImpl{
+		Cache:  rds,
+		Memory: mem,
+	}
+
+	paste := models.Item{
+		ID:    "wNnwj138ne9ZaWmNADwIg",
+		Paste: "Hello world!",
+		Hash:  "7e81ebe9e604a0c97fef0e4cfe71f9ba0ecba13332bde953ad1c66e4",
+		IP:    "127.0.0.1",
+		User:  "example@test.com",
+	}
+
+	err := p.InsertPasteToCache(paste)
+	if err != nil {
+		t.Error(err)
+	}
+}
