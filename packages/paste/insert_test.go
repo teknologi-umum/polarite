485aeb7db2dffb80efc7264921d325cec7b95ee1
Reinaldy Rafli <aldy505@tutanota.com>
2021-11-22 13:39:40 +0000

    feat: adjustment on generated paste

--- packages/paste/insert_test.go
+++ packages/paste/insert_test.go
@@ -2,8 +2,11 @@ package paste_test
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 	"polarite/packages/paste"
 	"testing"
+	"time"
 )
 
 func TestInsertPasteToDB(t *testing.T) {
@@ -16,6 +19,7 @@ func TestInsertPasteToDB(t *testing.T) {
 	}
 
 	paste := paste.Item{
+		ID:    "asdfgh",
 		Paste: []byte("Hello world!"),
 		Hash:  "7e81ebe9e604a0c97fef0e4cfe71f9ba0ecba13332bde953ad1c66e4",
 		IP:    "127.0.0.1",
@@ -29,6 +33,61 @@ func TestInsertPasteToDB(t *testing.T) {
 
 	if i.ID == "" {
 		t.Error("something went wrong, got:", i)
+	}
+}
+
+func TestInsertPasteToDB_Collision(t *testing.T) {
+	defer TruncateTable(db, rds, mem)
+
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(time.Second*30))
+	defer cancel()
+	c, err := db.Connx(ctx)
+	if err != nil {
+		t.Error(err)
+	}
+	defer c.Close()
+
+	item := paste.Item{
+		ID:    "asdfgh",
+		Paste: []byte("Hello world!"),
+		Hash:  "7e81ebe9e604a0c97fef0e4cfe71f9ba0ecba13332bde953ad1c63jk",
+		IP:    "127.0.0.1",
+		User:  "example@test.com",
+	}
+
+	tx, err := c.BeginTxx(ctx, &sql.TxOptions{})
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = tx.ExecContext(
+		ctx,
+		"INSERT INTO paste (id, content, hash, ip, user) VALUES (?, ?, ?, ?, ?)",
+		item.ID, item.Paste, item.Hash, item.IP, item.User)
+	if err != nil {
+		tx.Rollback()
+		t.Error(err)
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		tx.Rollback()
+		t.Error(err)
+	}
+
+	p := paste.Dependency{
+		Cache:  rds,
+		Memory: mem,
+		DB:     db,
+	}
+
+	i, err := p.InsertPasteToDB(context.Background(), item)
+	if err == nil {
+		t.Error("expected error, got:", i)
+	}
+
+	if err != nil && !errors.Is(err, paste.ErrIDDuplicate) {
+		t.Error(err)
 	}
 }
 
